744a214cff528353e69bda28c38dce2dfafebc48
Lucas Veiga <lucasveiga09@gmail.com>
2021-04-06 22:14:04 -0300

    Add Descending Selection Sort

--- src/main/java/com/grokkingalgorithms/SelectionSort.java
+++ src/main/java/com/grokkingalgorithms/SelectionSort.java
@@ -1,17 +1,34 @@
 package com.grokkingalgorithms;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
-    public static void sort(int[] arr) {
+    public static void sortAscending(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int smallest = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[smallest])
                     smallest = j;
             }
-            int temp = arr[i];
-            arr[i] = arr[smallest];
-            arr[smallest] = temp;
+            swap(arr, i, smallest);
         }
+    }
+
+    public static void sortDescending(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int highest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[highest])
+                    highest = j;
+            }
+            swap(arr, i, highest);
+        }
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
